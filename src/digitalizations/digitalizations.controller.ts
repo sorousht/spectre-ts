de0a289af849b3652f5f8977a7b024556ba04702
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2020-05-25 21:34:34 +0200

    Add an endpoint for getting all digitalizations

--- src/digitalizations/digitalizations.controller.ts
+++ src/digitalizations/digitalizations.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Digitalization } from './models/digitalization.model';
+import { DigitalizationsService } from './digitalizations.service';
 
 @Controller('digitalizations')
-export class DigitalizationsController {}
+export class DigitalizationsController {
+  constructor(private readonly service: DigitalizationsService) {}
+
+  @Get()
+  getAll(): Promise<Digitalization[]> {
+    return this.service.getAll();
+  }
+}
