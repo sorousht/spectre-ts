d18aa4d54b9149f5edb927887a30708b8e653b44
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2020-05-25 22:52:54 +0200

    Anotate models for graphql endpoint

--- src/digitalizations/models/digitalization.model.ts
+++ src/digitalizations/models/digitalization.model.ts
@@ -1,11 +1,22 @@
 import { States } from './state';
 import { Channel } from './channel';
 import { Ocr } from './ocr';
+import { ObjectType, Field, ID, Int } from '@nestjs/graphql';
 
+@ObjectType()
 export class Digitalization {
+  @Field(type => ID)
   _id: string;
+
+  @Field(type => Int, { description: 'A unique number', deprecationReason: 'Use sequenceNumber instead' })
   sequenceId: Number;
+
+  @Field({ description: 'The current state of the digitalization' })
   state: States;
+  
+  @Field({ description: 'The channel through which the digitalization has been imported' })
   channel: Channel;
-  ocr?: Ocr
+  
+  @Field({ nullable: true, description: 'The result of OCR robot' })
+  ocr?: Ocr;
 }
--- src/digitalizations/models/ocr.ts
+++ src/digitalizations/models/ocr.ts
@@ -1,5 +1,13 @@
+import { ObjectType, Field } from "@nestjs/graphql";
+
+@ObjectType()
 export class Ocr {
+  @Field()
   wasSuccessful: boolean;
+
+  @Field()
   finishedBy: string;
+
+  @Field()
   finishedAt: Date;
 };
